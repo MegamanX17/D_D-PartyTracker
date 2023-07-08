4355710d29682ebd6d3610709da5148ea9922e6f
Norlan Castillo <ncast133@fiu.edu>
2023-07-08 03:58:50 -0400

    First commit
    This project currently contains a single windows form that utilizes a numericupdown to keep track of current players. The window is able to be properly closed down.

---
+++ D-PartyTracker/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace D_PartyTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.EnterBttn = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F);
+            this.label1.Location = new System.Drawing.Point(175, 205);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(765, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to the Party Tracker!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.label2.Location = new System.Drawing.Point(203, 331);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(573, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please enter the number of players:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.numericUpDown1.AutoSize = true;
+            this.numericUpDown1.Location = new System.Drawing.Point(765, 334);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 38);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.TabStop = false;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // EnterBttn
+            // 
+            this.EnterBttn.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.EnterBttn.Location = new System.Drawing.Point(273, 416);
+            this.EnterBttn.Name = "EnterBttn";
+            this.EnterBttn.Size = new System.Drawing.Size(240, 71);
+            this.EnterBttn.TabIndex = 3;
+            this.EnterBttn.Text = "Enter";
+            this.EnterBttn.UseVisualStyleBackColor = true;
+            this.EnterBttn.Click += new System.EventHandler(this.EnterBttn_Click);
+            // 
+            // Close
+            // 
+            this.Close.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Close.Location = new System.Drawing.Point(602, 416);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(212, 71);
+            this.Close.TabIndex = 4;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(240F, 240F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1168, 712);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.EnterBttn);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(1200, 800);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button EnterBttn;
+        private System.Windows.Forms.Button Close;
+    }
+}
+
---
+++ D-PartyTracker/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D_PartyTracker
+{
+    public partial class Form1 : Form
+    {
+        // private TextBox txt;
+        // private Button lck;
+        int numPlayers = 0; // counter for the current number of players
+        // int numLabels = 1; // counter for current number of label objects
+        // int numTextBox = 1; // counter for current number of text box objects
+        // int numBttn = 1; // counter for current number of button objects
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            // Closes the entire form window
+            Application.Exit();
+        }
+
+        /**
+         * This method controls the behavior of the "Enter" button and
+         * calls the constructor of the next form.
+         */
+        private void EnterBttn_Click(object sender, EventArgs e)
+        {
+            // Stores the current number of players entered in the numericUpDown
+            numPlayers = Convert.ToInt32(numericUpDown1.Value);
+
+
+
+
+
+            /*
+            // clears everything on the current windows form
+            this.Controls.Clear();
+
+            // this loop creates a new label and a new text box
+            // based on the current number of players
+            for (int i = 0; i < numPlayers; i++)
+            {
+                addButton(); // calls the add button method
+                addLabel(); // calls the add label method
+                addText(); // calls the add text method
+            }
+            addEnterButton(); // calls the add "Enter" button method
+            //addClearButton(); // calls the add "Clear" button method
+            */
+        }
+
+
+
+        //The sections of code beyond this point has been commented and will no longer be used.
+        //The commented sections of coding will not be removed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /**
+         * The addText method creates a new text box object and adds that object
+         * to the currently active window form.
+         */
+        /*private System.Windows.Forms.TextBox addText()
+        {
+            // new text box object is constructed
+            txt = new TextBox();
+
+
+            // new text box object is added to the form
+            this.Controls.Add(txt); 
+
+            // changes what is said inside the text box
+            //txt.Text = "TextBox #" + this.numTextBox.ToString();
+
+            // spacing between the text box and the top of the window form
+            txt.Top = numTextBox * 40;
+
+            // spacing between the text box and the left side of the window form
+            txt.Left = 195;
+
+            // increases the total number of current text boxes made by 1
+            numTextBox++;
+            return txt;
+        }*/
+
+        /**
+         * The addLabel method creates a new label object and adds that object
+         * to the currently active window form.
+         */
+        /*private System.Windows.Forms.Label addLabel()
+        {
+            // new label object is constructed
+            Label lab = new Label();
+
+            // new size is set for the label object
+            lab.Size = new Size(95, 15);
+
+            // label object is added to the current window form
+            this.Controls.Add(lab);
+
+            // changes what is said on the label
+            lab.Text = "Player #" 
+                + this.numLabels.ToString() + "'s name:";
+
+            // spacing between the label and the top side of the window form
+            lab.Top = numLabels * 40;
+
+            // spacing between the label and the left side of the window form
+            lab.Left = 100;
+
+            // increases the total number of current labels made by 1
+            numLabels++;
+            return lab;
+        }*/
+
+
+        /**
+         * The addButton method creates a new button object and adds that object
+         * to the currently active window form.
+         */
+        /*private System.Windows.Forms.Button addButton()
+        {
+            lck = new Button();
+
+            lck.Click += Lock_Click;
+
+            this.Controls.Add(lck);
+
+            lck.Text = "Lock";
+
+            lck.Top = numTextBox * 40;
+
+            lck.Left = 300;
+
+            return lck;
+        }*/
+
+
+        /*private System.Windows.Forms.Button addEnterButton()
+        {
+            // new button object is constructed
+            Button btn = new Button();
+
+            btn.Click += Button_Click;
+
+            // new button object is added to the window form
+            this.Controls.Add(btn);
+
+            // changes what is said on the button
+            btn.Text = "Enter";
+
+            btn.Top = numTextBox * 42;
+
+            btn.Left = 175;
+
+            // increases the total number of current buttons made by 1
+            //numBttn++;
+            return btn;
+        }*/
+
+
+        /*
+        private System.Windows.Forms.Button addClearButton()
+        {
+            Button btn = new Button();
+
+            this.Controls.Add(btn);
+
+            btn.Text = "Clear";
+
+            btn.Top = numTextBox * 45;
+
+            btn.Left = 225;
+
+            return btn;
+        }
+        */
+
+
+        /*private void Button_Click(object sender, EventArgs e)
+        {
+            // clears everything on the current windows form
+            this.Controls.Clear();
+        }*/
+
+
+        /*private void Lock_Click(object sender, EventArgs e)
+        {
+            if (lck.Text == "Lock")
+            {
+                txt.ReadOnly = true;
+                lck.Text = "Unlock";
+            } 
+            
+            else
+            {
+                txt.ReadOnly = false;
+                lck.Text = "Lock";
+            }
+        }*/
+    }
+}
