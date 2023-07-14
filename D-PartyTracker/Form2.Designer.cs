6625b07736a1bc37f0715fbe07efb1646a771084
Norlan Castillo <ncast133@fiu.edu>
2023-07-13 23:12:50 -0400

    Form 2 player list
    Added code and functionality to form #2. Player information and names can now be saved. Also added some error handling, and made it possible to close the program from form #2.

--- D-PartyTracker/Form2.Designer.cs
+++ D-PartyTracker/Form2.Designer.cs
@@ -32,6 +32,11 @@
             this.PlayerList = new System.Windows.Forms.RichTextBox();
             this.PlayerContent = new System.Windows.Forms.TextBox();
             this.PlayerInfo = new System.Windows.Forms.Label();
+            this.EnterBttn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.DoneBttn = new System.Windows.Forms.Button();
+            this.CloseBttn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -50,6 +55,7 @@
             this.PlayerList.Font = new System.Drawing.Font("Times New Roman", 12F);
             this.PlayerList.Location = new System.Drawing.Point(33, 137);
             this.PlayerList.Name = "PlayerList";
+            this.PlayerList.ReadOnly = true;
             this.PlayerList.Size = new System.Drawing.Size(325, 543);
             this.PlayerList.TabIndex = 2;
             this.PlayerList.TabStop = false;
@@ -58,7 +64,8 @@
             // PlayerContent
             // 
             this.PlayerContent.Font = new System.Drawing.Font("Times New Roman", 12F);
-            this.PlayerContent.Location = new System.Drawing.Point(407, 206);
+            this.PlayerContent.Location = new System.Drawing.Point(407, 273);
+            this.PlayerContent.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
             this.PlayerContent.Name = "PlayerContent";
             this.PlayerContent.Size = new System.Drawing.Size(312, 53);
             this.PlayerContent.TabIndex = 3;
@@ -69,16 +76,63 @@
             this.PlayerInfo.AutoSize = true;
             this.PlayerInfo.Font = new System.Drawing.Font("Times New Roman", 12F);
             this.PlayerInfo.Location = new System.Drawing.Point(399, 137);
+            this.PlayerInfo.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
             this.PlayerInfo.Name = "PlayerInfo";
             this.PlayerInfo.Size = new System.Drawing.Size(117, 46);
             this.PlayerInfo.TabIndex = 4;
             this.PlayerInfo.Text = "label2";
+            // 
+            // EnterBttn
+            // 
+            this.EnterBttn.Location = new System.Drawing.Point(769, 273);
+            this.EnterBttn.Name = "EnterBttn";
+            this.EnterBttn.Size = new System.Drawing.Size(125, 62);
+            this.EnterBttn.TabIndex = 5;
+            this.EnterBttn.Text = "Enter";
+            this.EnterBttn.UseVisualStyleBackColor = true;
+            this.EnterBttn.Click += new System.EventHandler(this.EnterBttn_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.DoneBttn, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.CloseBttn, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(407, 403);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(487, 161);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // DoneBttn
+            // 
+            this.DoneBttn.Location = new System.Drawing.Point(3, 3);
+            this.DoneBttn.Name = "DoneBttn";
+            this.DoneBttn.Size = new System.Drawing.Size(237, 155);
+            this.DoneBttn.TabIndex = 0;
+            this.DoneBttn.Text = "Done";
+            this.DoneBttn.UseVisualStyleBackColor = true;
+            // 
+            // CloseBttn
+            // 
+            this.CloseBttn.Location = new System.Drawing.Point(246, 3);
+            this.CloseBttn.Name = "CloseBttn";
+            this.CloseBttn.Size = new System.Drawing.Size(238, 155);
+            this.CloseBttn.TabIndex = 1;
+            this.CloseBttn.TabStop = false;
+            this.CloseBttn.Text = "Close";
+            this.CloseBttn.UseVisualStyleBackColor = true;
+            this.CloseBttn.Click += new System.EventHandler(this.CloseBttn_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1168, 712);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.EnterBttn);
             this.Controls.Add(this.PlayerInfo);
             this.Controls.Add(this.PlayerContent);
             this.Controls.Add(this.PlayerList);
@@ -87,6 +141,7 @@
             this.MinimumSize = new System.Drawing.Size(1200, 800);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,5 +153,9 @@
         private System.Windows.Forms.RichTextBox PlayerList;
         private System.Windows.Forms.TextBox PlayerContent;
         private System.Windows.Forms.Label PlayerInfo;
+        private System.Windows.Forms.Button EnterBttn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button DoneBttn;
+        private System.Windows.Forms.Button CloseBttn;
     }
 }
--- D-PartyTracker/Form2.cs
+++ D-PartyTracker/Form2.cs
@@ -14,13 +14,60 @@ namespace D_PartyTracker
     {
         int numPlayers = 0; // counter for the total number of players
         int currPlayer = 0; // counter for the current player
+        bool containsInt = false; // boolean that checks for a string containing a number
         public Form2(int numPlayers)
         {
             this.numPlayers = numPlayers; // saves the number of players passed by Form 1
             InitializeComponent();
             label1.Text += " " + numPlayers; // displays the number of players on label 1
-            PlayerInfo.Text = "Please type player #" + (currPlayer + 1) + "'s name below";
+            PlayerInfo.Text = "Please type player #" + (currPlayer + 1) + "'s name " +
+                "\nand max HP below (Ex: Roger, 25)";
         }
 
+        private void EnterBttn_Click(object sender, EventArgs e)
+        {
+            
+            // checks to see if the user wrote a number
+            containsInt = (PlayerContent.Text).Any(char.IsDigit);
+
+            // this if statement checks for a blank string entered by a user
+            // and checks if there is a number being entered (error handling)
+            if (PlayerContent.Text.Equals("") || (containsInt == false))
+            {
+                MessageBox.Show("Please enter an actual name with hp!", "D&D PartyTracker",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // this else-if statement saves a string entered by the user to the richtextbox
+            // only if the names entered are not empty,
+            // doesn't exceed the total number of players, and contains a number
+            else if (currPlayer < numPlayers && !(PlayerContent.Text).Equals("") 
+                && (containsInt == true))
+            {
+                PlayerList.Text += PlayerContent.Text + "\n"; // passes a name to the richtextbox list
+                PlayerContent.Text = ""; // empties the textbox being used to write names
+                currPlayer++; // increments current player being looked at
+            }
+
+            // this else-if statement prevents newer names from being entered if
+            // all players have already received a name (error handling)
+            else if(currPlayer == numPlayers)
+            {
+                MessageBox.Show("No more names can be entered!", "D&D PartyTracker",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (currPlayer < numPlayers)
+            {
+                PlayerInfo.Text = "Please type player #" + (currPlayer + 1) + "'s name " +
+                "\nand max HP below (Ex: Roger, 25)"; // updates what is said on the form
+            }
+
+        }
+
+        private void CloseBttn_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // closes the entire program
+        }
     }
 }
